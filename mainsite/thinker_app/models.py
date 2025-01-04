fc51f86750829bf8f0656e8ddd8c77bcbcb54786
Remco <remcoveens03@gmail.com>
2025-01-04 17:03:41 +0100

    first changes

--- mainsite/thinker_app/models.py
+++ mainsite/thinker_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Thought(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    msg = models.CharField(verbose_name="message", max_length=255)
+    img = models.ImageField(upload_to="posts",blank=True)
+
+    def __str__(self):
+        return f"Thought #{self.id}"
--- mainsite/thinker_app/views.py
+++ mainsite/thinker_app/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
-# Create your views here.
+from .models import Thought
+
 class home(TemplateView):
     template_name= "base/home.html"
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context["thoughts"] = Thought.objects.all()
+        return context
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
